4e4aaf61dd83fc8c069468ed0a5b03d28d5bdd82
zanekhankan <zanekhan077@gmail.com>
2025-06-24 19:36:00 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,9 @@
+# ZGZO.AI GC Config Creator
+
+This app lets General Contractors create branded configuration profiles that plug into the ZGZO.AI AI bidding engine.
+
+## How to Use
+
+1. Open the app
+2. Enter GC info (license, markup, branding)
+3. Save profile
